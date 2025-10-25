5aca7b91fec0a9858467a1c927e2f0fbcb6e3ce6
edy <0roroe13@solent.ac>
2025-10-25 16:24:37 +0100

    finnished the day

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,10 +1,15 @@
-class Cat (val name: String, var age: Int, var weight: Int) {
-
+class Cat (val name: String, var age: Int, var initilweight: Int) {
+var weight:Int = initilweight
+    set(value) {
+        if (value in 0..20) field = value
+        else println("The weight must be between 0 and 20, the cat is to fat")
+    }
     fun walk(times: Int = 1) {
         weight = weight-times
     }
     fun eat(times: Int = 1) {
         weight = weight+times
+        if (weight > 20) weight = 20
     }
 
     override fun toString() : String {
--- src/main.kt
+++ src/main.kt
@@ -5,7 +5,7 @@ fun main() {
     val cupra = Cat("Cupra", 2, 7)
     binnie.walk()
     clyde.walk()
-    flathead.eat(2)
+    flathead.eat(11)
 
     cupra.walk(4)
 
