c7ed9e67894098812148c4cc86a04d326d35d004
edy <0roroe13@solent.ac>
2025-10-25 15:19:57 +0100

    added eat function

--- src/Cat.kt
+++ src/Cat.kt
@@ -3,8 +3,17 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     fun walk() {
         weight--
     }
+    fun eat() {
+        weight++
+    }
 
     override fun toString() : String {
         return "Name: $name Age: $age"
+    }
+
+
+
+    override fun toSrting(): String {
+        return "Name: $name age: $age weight: $weight"
     }
 }
